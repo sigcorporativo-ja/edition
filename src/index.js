6e82ff704b44e8252f82bdd942e00b4389acdbae
irenevinas <irenevinas@guadaltel.com>
2020-06-23 10:50:27 +0200

    Acualizado api.json

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import M$plugin$Edition from '/home/irenevinas/m5/mapea-publico/src/plugins/v5/edition/src/facade/js/edition';
-
-if (!window.M.plugin) window.M.plugin = {};
-window.M.plugin.Edition = M$plugin$Edition;
